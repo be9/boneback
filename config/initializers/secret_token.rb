4707b88573e7f04996c4453527faef31afab02ae
oleg dashevskii <olegdashevskii@gmail.com>
2011-07-09 14:47:34 +0700

    Initial

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Boneback::Application.config.secret_token = '5cd564fbf803c51cd57b39e2e2870f2ac42d069c32f6966c88d81469f9f5c0096e55c241c462f3cffb4fbba980c7cae60edbbf9bda732692c643c0c01e0fafcd'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,8 @@
+# Be sure to restart your server when you modify this file.
+
+Boneback::Application.config.session_store :cookie_store, :key => '_boneback_session'
+
+# Use the database for sessions instead of the cookie-based default,
+# which shouldn't be used to store highly confidential information
+# (create the session table with "rails generate session_migration")
+# Boneback::Application.config.session_store :active_record_store
